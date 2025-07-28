3d38fca1a47922a68cd88f92823bdfbb5f2ffceb
Gyula Asztalos <137063656+gyulaasztalos@users.noreply.github.com>
2025-07-28 16:42:54 +0200

    paperless-ngx init commit

---
+++ apps/paperless-ngx/install/notification.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DATE="$(date '+%Y.%m.%d %H:%M')"
+
+BODY="[paperless] [✅] A document ${DOCUMENT_ORIGINAL_FILENAME} was just consumed. Correspondent - ${DOCUMENT_CORRESPONDENT} || Tags - ${DOCUMENT_TAGS}"
+
+TYPE="success"
+TITLE="[k3s] Successfull document consumption"
+TAG="paperless"
+
+DATA='{"tag":"'${TAG}'","type":"'${TYPE}'","body":"'${BODY}'","title":"'${TITLE}'"}'
+
+curl -X POST --data "${DATA}" -H "Content-Type: application/json" "http://apprise.apprise.svc.cluster.local:8000/notify/apprise"
+
+exit $?
